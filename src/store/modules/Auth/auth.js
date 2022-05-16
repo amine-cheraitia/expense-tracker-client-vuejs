1c975a1c00f836571d4898a94572da80e6fe92d2
Amine Cheraitia <amine-cheraitia@hotmail.com>
2022-05-16 17:35:40 +0200

    setup store auth and ressources for user which is logged

--- src/store/modules/Auth/auth.js
+++ src/store/modules/Auth/auth.js
@@ -1,11 +1,17 @@
 export default {
+	namespaced: true,
 	state() {
 		return {
+			userId: 1,
 			username: "",
 			token: null,
 		};
 	},
 	mutations: {},
 	actions: {},
-	getters: {},
+	getters: {
+		userId(state) {
+			return state.userId;
+		},
+	},
 };
--- src/store/modules/ressources/ressources.js
+++ src/store/modules/ressources/ressources.js
@@ -1,17 +1,45 @@
+import axios from "axios";
+
 export default {
+	namespaced: true,
 	state() {
 		return {
 			solde: 0,
 			ressources: [
 				{
 					id: 5,
-					num_compte: "random",
-					nom_ressource: "nom",
 				},
 			],
 		};
 	},
-	mutations: {},
-	actions: {},
-	getters: {},
+	mutations: {
+		setRessources(state, payload) {
+			/* state.ressources.push(...payload); version qui nest ok 100*/
+			state.ressources = [...payload];
+		},
+	},
+	actions: {
+		async loadRessources(context) {
+			let ressources;
+			const userId = context.rootGetters["auth/userId"];
+			await axios
+				.get("http://127.0.0.1:8000/api/ressource/user/" + userId)
+				.then((result) => {
+					ressources = result.data;
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+
+			context.commit("setRessources", ressources);
+		},
+	},
+	getters: {
+		solde(state) {
+			return state.solde;
+		},
+		ressources(state) {
+			return state.ressources;
+		},
+	},
 };

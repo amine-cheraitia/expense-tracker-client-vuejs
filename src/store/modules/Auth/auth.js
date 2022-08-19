0e4e00b6b584c907d23ff108ce740ecb90b2c454
Amine Cheraitia <amine-cheraitia@hotmail.com>
2022-08-19 18:59:00 +0200

    set logout in store

--- src/store/modules/Auth/auth.js
+++ src/store/modules/Auth/auth.js
@@ -3,12 +3,25 @@ export default {
 	state() {
 		return {
 			userId: 1,
-			username: "",
+			user: null,
 			token: null,
 		};
 	},
-	mutations: {},
-	actions: {},
+	mutations: {
+		resetUser(state) {
+			state.userId = null;
+			state.user = null;
+			state.token = null;
+		},
+	},
+	actions: {
+		async logout(context) {
+			localStorage.removeItem("userId");
+			localStorage.removeItem("user");
+			localStorage.removeItem("token");
+			await context.commit("resetUser");
+		},
+	},
 	getters: {
 		userId(state) {
 			return state.userId;

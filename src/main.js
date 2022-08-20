c8e9732272e25b7c0a64a231b8d5e56965bcddb8
Amine Cheraitia <amine-cheraitia@hotmail.com>
2022-08-20 21:12:06 +0200

    adding baseURL to main js

--- src/main.js
+++ src/main.js
@@ -5,5 +5,8 @@ import store from "./store";
 import "@fortawesome/fontawesome-free/js/all";
 import VueSweetalert2 from "vue-sweetalert2";
 import "sweetalert2/dist/sweetalert2.min.css";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:8000";
 
 createApp(App).use(store).use(router).use(VueSweetalert2).mount("#app");

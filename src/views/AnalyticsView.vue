f59754cc25519e6d504c7b963d0d064f2a86930e
Amine Cheraitia <amine-cheraitia@hotmail.com>
2022-08-22 17:48:21 +0200

    setting up beforeMount & beforeCreate with checktoken actions

--- src/views/AnalyticsView.vue
+++ src/views/AnalyticsView.vue
@@ -6,6 +6,14 @@
 export default {
 	name: "name",
 	props: {},
+	beforeMount() {
+		console.log("before mount");
+		this.$store.dispatch("auth/checkToken");
+	},
+	beforeCreate() {
+		console.log("before create");
+		this.$store.dispatch("auth/checkToken");
+	},
 };
 </script>
 

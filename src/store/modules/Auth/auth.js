87fb0d57c6cf1a5cfa4334365ef78d36ce3461ab
Amine Cheraitia <amine-cheraitia@hotmail.com>
2022-08-22 17:45:51 +0200

    edit actions (tryLogin (with router push)) and adding checkToken

--- src/store/modules/Auth/auth.js
+++ src/store/modules/Auth/auth.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import router from "@/router";
 
 export default {
 	namespaced: true,
@@ -52,14 +53,54 @@ export default {
 					throw err.response.data.message;
 				});
 		},
-		tryLogin(context) {
+		async tryLogin({ commit }) {
 			const userId = localStorage.getItem("userId");
 			const user = JSON.parse(localStorage.getItem("user"));
 			const token = localStorage.getItem("token");
-			console.log(user);
-			if (user && token) {
-				context.commit("setUser", { userId: userId, user: user, token: token });
-			}
+
+			const config = {
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			};
+			await axios
+				.post("/api/checktoken", user, config)
+				.then((res) => {
+					console.log(res);
+					if (user && token) {
+						commit("setUser", { userId: userId, user: user, token: token });
+						router.push("/");
+					}
+				})
+				.catch((err) => {
+					console.log(err);
+					commit("resetUser");
+					router.push("/login");
+				});
+		},
+		async checkToken({ state, commit }) {
+			const config = {
+				headers: {
+					Authorization: `Bearer ${state.token}`,
+				},
+			};
+
+			await axios
+				.post("/api/checktoken", state.user, config)
+				.then((res) => {
+					console.log(res);
+					/* 					if (user && token) {
+						commit("setUser", { userId: userId, user: user, token: token });
+						router.push("/");
+					} */
+				})
+				.catch((err) => {
+					console.log(err);
+
+					commit("resetUser");
+
+					router.push("/login");
+				});
 		},
 	},
 	getters: {

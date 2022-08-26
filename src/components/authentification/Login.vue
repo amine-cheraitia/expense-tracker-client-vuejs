0d2f67fdda42fb83a18d105426c0480b4f1e9c61
Amine Cheraitia <amine-cheraitia@hotmail.com>
2022-08-26 22:55:51 +0200

    add login and signup component to nest both of them in same auth component

---
+++ src/components/authentification/Login.vue
@@ -0,0 +1,34 @@
+<template>
+	<form action="" method="post" @submit.prevent="sendCridential">
+		<div class="card">
+			<div class="title">
+				<i class="fa-solid fa-chart-column"></i> Expense Tracker
+			</div>
+			<div class="form-container" :class="{ invalid: Errors.email }">
+				<label for="email">Email</label>
+				<input type="email" id="email" v-model="form.email" @change="reset" />
+			</div>
+			<div class="form-container" :class="{ invalid: Errors.password }">
+				<label for="">Password</label>
+				<input type="password" v-model="form.password" @change="reset" />
+			</div>
+			<div
+				class="form-control invalidTxt"
+				style="margin-top: 10px"
+				v-if="Errors.email || Errors.password"
+			>
+				<span>{{ messageError }}</span>
+			</div>
+			<button class="btn">Se Connecter</button>
+		</div>
+	</form>
+</template>
+
+<script>
+export default {
+	name: "name",
+	props: {},
+};
+</script>
+
+<style scoped></style>

c29bdfd2cca48c0ff76c384a9e842b4df5868a4d
Amine Cheraitia <amine-cheraitia@hotmail.com>
2022-08-28 21:13:20 +0200

    setting up first part of main dashboard

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,18 @@
 		<!-- 		<img alt="Vue logo" src="../assets/logo.png" />
 		<HelloWorld msg="Welcome to Your Vue.js App" /> -->
 		<div class="number-section">
-			<div class="number-box">1</div>
-			<div class="number-box">2</div>
-			<div class="number-box">3</div>
+			<div class="number-box">
+				<div>Montant des entrés total :</div>
+				<span class="money solde">50 000.00 DA</span>
+			</div>
+			<div class="number-box">
+				<div>Montant des sortie total :</div>
+				<span class="money minus">-50 000.00 DA</span>
+			</div>
+			<div class="number-box">
+				<div>Le solde total :</div>
+				<span class="money plus">50 000.00 DA</span>
+			</div>
 		</div>
 		<div class="chart-section">
 			<div class="chart-box">chart1</div>
@@ -18,6 +27,7 @@
 <script>
 // @ is an alias to /src
 /* import HelloWorld from "@/components/HelloWorld.vue"; */
+/* import { BarChart } from "@/components/Charts/BarChart.vue"; */
 
 export default {
 	name: "HomeView",
@@ -46,9 +56,40 @@ export default {
 	width: 90%;
 	height: 150px;
 	margin-bottom: 20px;
+	display: flex;
+	align-items: center;
+	justify-content: space-evenly;
 }
 .chart-section {
 	width: 90%;
 	height: 350px;
+}
+.number-box {
+	padding: 15px;
+	border: #9c88ff 0.5px solid;
+	width: 280px;
+	height: 70px;
+	flex: 0 0 280px;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+}
+
+.money {
+	font-size: 20px;
+	letter-spacing: 1px;
+	margin: 5px 0;
+}
+
+.money.plus {
+	color: #2ecc71;
+}
+
+.money.minus {
+	color: #c0392b;
+}
+.money.solde {
+	font-weight: 700;
 }
 </style>

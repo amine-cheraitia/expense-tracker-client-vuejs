777184936181e396a5c5f0cd738ae256e273766c
Amine Cheraitia <amine-cheraitia@hotmail.com>
2022-09-04 21:34:53 +0200

    adding to the mouvement store : actions and mutations for monthly kpi

--- src/store/modules/mouvements/mouvements.js
+++ src/store/modules/mouvements/mouvements.js
@@ -1,10 +1,11 @@
 import axios from "axios";
+
 export default {
 	namespaced: true,
 	state() {
 		return {
 			mouvements: [
-				{
+				/* 				{
 					id: 1,
 					description: "Cash ",
 					montant: 500,
@@ -21,10 +22,12 @@ export default {
 					type_mouvement_id: 2,
 					user_id: 1,
 					ressource_id: 3,
-				},
+				}, */
 			],
 			totalEntré: 0,
 			totalSortie: 0,
+			monthly_kpi_recette: null,
+			monthly_kpi_depense: null,
 		};
 	},
 	mutations: {
@@ -35,8 +38,46 @@ export default {
 			state.totalEntré = payload.entré;
 			state.totalSortie = payload.sortie;
 		},
+		set_monthly_kpi(state, payload) {
+			state.monthly_kpi_recette = [...payload.recette];
+			state.monthly_kpi_depense = [...payload.depense];
+			console.log(state.monthly_kpi_recette);
+		},
 	},
 	actions: {
+		async loadKpi(context) {
+			const userid = context.rootGetters["auth/userId"];
+			/* 			const config = {
+				headers: {
+					Authorization: `Bearer ${context.rootGetters["auth/getToken"]}`,
+				},
+			}; */
+			/* Todo call api and retrive data for monthly and yearly kpi */
+			const depense = [];
+			const recette = [];
+			for (let index = 0; index < 12; index++) {
+				depense.push(0);
+				recette.push(0);
+			}
+			console.log(depense);
+			await axios(`http://127.0.0.1:8000/api/mouvement/kpi/${userid}`)
+				.then((res) => {
+					console.log(res.data);
+					res.data.forEach((mouvement) => {
+						for (let key in mouvement) {
+							console.log(`${key}: ${mouvement[key].m}`);
+							depense[mouvement[key].m - 1] = parseInt(mouvement[key].depense);
+							recette[mouvement[key].m - 1] = parseInt(mouvement[key].recette);
+							console.log(recette);
+						}
+						context.commit("set_monthly_kpi", {
+							depense: depense,
+							recette: recette,
+						});
+					});
+				})
+				.catch((err) => err);
+		},
 		async loadEntréSortie(context) {
 			const userId = context.rootGetters["auth/userId"];
 			const config = {

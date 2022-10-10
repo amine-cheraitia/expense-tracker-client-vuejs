cdccb387acbf90a05b9c4d2367aaae6c4ec34c4a
Amine Cheraitia <amine-cheraitia@hotmail.com>
2022-10-10 13:32:37 +0200

    adding new component breadcrumb

---
+++ src/components/ui/Breadcrumb.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<div class="breadcrumb">
+			<router-link to="/" style="text-align: left">Tableau de bord</router-link>
+			/ {{ this.pageName }}
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: ["pageName"],
+};
+</script>
+
+<style scoped>
+.breadcrumb {
+	text-align: left;
+	margin-left: 65px;
+	color: #2c3e50;
+	font-weight: 400;
+}
+a {
+	text-decoration: none;
+	color: #6d5eb4;
+}
+@media (max-width: 550px) {
+	.breadcrumb {
+		margin-left: 35px;
+	}
+}
+</style>
